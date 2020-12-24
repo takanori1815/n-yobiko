a6003fcd35b3b1fdb80d3f5c03fe06b735d22d37
takanori1815 <takakki2810@gmail.com>
2020-12-24 17:21:39 +0900

    初めてのコミット

---
+++ workspace/hubot-todo/scripts/todo.js
@@ -0,0 +1,14 @@
+// Description:
+//   TODO を管理することができるボットです
+// Commands:
+//   ボット名 todo     - TODO を作成
+//   ボット名 done     - TODO を完了にする
+//   ボット名 del      - TODO を消す
+//   ボット名 list     - TODO の一覧表示
+//   ボット名 donelist - 完了した TODO の一覧表示
+'use strict';
+const todo = require('todo');
+module.exports = robot => {};'use strict';
+const todo = require('todo');
+
+module.exports = robot => {};
